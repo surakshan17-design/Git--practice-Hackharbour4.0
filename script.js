4e96e5cc7560b166b6e57e3260a69a49d20f7114
Suraksha Naik <surakshan17@gmail.com>
2026-08-20 12:10:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const photo = document.getElementById("myPhoto");
-const button = document.getElementById("changePhotoBtn");
-button.addEventListener("click", function () {
-   photo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrb4OvIZOz-Z2RvlJ0xDl1E_e3qOfh_TQK1va1Z7gJ4g&s=10";
-    photo.alt = "My Photo";
-    console.log("Photo changed successfully!");
-});
